6d07d0ca389491f1b2082e4decf8ca94cfebb4c8
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-02 03:00:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ require "pry"
 
 def join_nested_strings(src)
   counter = 0
-  while counter < src.length do
+  while counter < src.count do
     inner_counter = 0
     while inner_counter < src[counter][inner_counter].length do
       # binding.pry

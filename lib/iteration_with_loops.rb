724593e542dafb4e44b0b642eba4fc1cfb19209a
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-02 02:58:25 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
     inner_counter = 0
     while inner_counter < src[counter][inner_counter].length do
       binding.pry
-      if src[counter][inner_counter].Class == String
+      if src[counter][inner_counter].Class == "String"
           p src[counter][inner_counter]
       end
       inner_counter += 1
